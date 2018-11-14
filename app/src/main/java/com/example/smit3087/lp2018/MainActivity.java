2ac011f608d387c81df0a4ebf230ed872ccafa52
asmith7899 <smit3087@purdue.edu>
2018-11-13 19:12:20 -0500

    Started implementing directions API - currently working on method to retrive/update current location. Lots of comments, so hopefully that clears stuff up. There are errors/placeholders in this code - ignore them!

--- app/src/main/java/com/example/smit3087/lp2018/MainActivity.java
+++ app/src/main/java/com/example/smit3087/lp2018/MainActivity.java
@@ -1,6 +1,13 @@
 package com.example.smit3087.lp2018;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -22,6 +29,12 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.maps.DirectionsApi;
+import com.google.maps.model.DirectionsResult;
+import com.google.maps.model.TravelMode;
+
+import org.joda.time.DateTime;
+
 public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
@@ -29,11 +42,53 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
     private static final int PLACE_PICKER_REQUEST = 1;
     private static final int AUTOCOMPLETE_REQUEST_CODE = 1;
     private String TAG = "";
+    private LocationManager locationManager;
+    private LocationListener locationListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            //if GPS is disabled
+            public void onProviderDisabled(String provider) {
+                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(intent);
+            }
+        };
+        //checks permissions for location request
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) !=
+                PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[] {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.INTERNET}, 12);
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        //this line updates out location constantly
+        locationManager.requestLocationUpdates("gps", 5000, 0, locationListener);
         mapView = findViewById(R.id.map);
         mapView.onCreate(savedInstanceState);
         mapView.onResume();
@@ -42,6 +97,17 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
 //        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
 //                .findFragmentById(R.id.map);
 //        mapFragment.getMapAsync(this);
+    }
+    @Override
+    //checks permissions
+    public void onRequestPermissionsResult(int requestCode, String []permissions, int[] grantResults) {
+        switch (requestCode) {
+            case 12:
+                //if permission is granted
+                if (grantResults.length >0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                }
+        }
     }
 
 
@@ -58,7 +124,6 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
@@ -85,14 +150,24 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
         }
     }
 
-    //this code retreives the user's chose place
+    //this code retrieves the user's chosen place
 
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        //need to initialize this DateTime
+        DateTime now = new DateTime();
         if (requestCode == PLACE_PICKER_REQUEST) {
             if (resultCode == RESULT_OK) {
                 Place place = PlacePicker.getPlace(data, this);
                 String toastMsg = String.format("Place: %s", place.getName());
                 Toast.makeText(this, toastMsg, Toast.LENGTH_LONG).show();
+
+                //I am not 100% sure at that this directions method is in the right place
+                //also I need to define origin, destination and figure out getGeoContext
+                //I set mode to walking automatically, Idk if that's ok
+                //can I use place.toString()?
+                DirectionsResult result = DirectionsApi.newRequest().mode
+                        (TravelMode.WALKING).origin(origin).destination(place.toString()).departureTime(now)
+
             }
         }
         if (requestCode == AUTOCOMPLETE_REQUEST_CODE) {
@@ -101,23 +176,30 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
                 //I couldn't find the TAG variable in the generated java, so I made a new null String.
                 //I'm not sure what TAG is supposed to be, so I don't know how important it is that it's null
                 Log.i(TAG, "Place: " + place.getName());
-
+       DirectionsResult result = DirectionsApi.newRequest(getGeoContext()).mode
+                        (TravelMode.WALKING).origin(origin).destination(place.toString()).departureTime(now)
                 //After this I should handle result error and result cancelled - the above only handles valid places
             }
         }
     }
+
+    //this code create my options menu
     public boolean onCreateOptionsMenu(Menu menu) {
         new MenuInflater(this).inflate(R.menu.options_menu, menu);
         return (super.onCreateOptionsMenu(menu));
     }
+    //this code repsonds to options sbeing clicked (launches new activities)
     public boolean onOptionsItemSelected(MenuItem item) {
+        //if user selects 'call safewalk'
         if (item.getItemId() == R.id.call_SW) {
 
         }
+        //plan trip
         if (item.getItemId() == R.id.plan_trip) {
             startActivity(new Intent(MainActivity.this, Main_to_Plan.class));
             return true;
         }
+        //text buddy
         if (item.getItemId() == R.id.text_friend) {
 
         }
